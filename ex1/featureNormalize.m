8c1c0cf1ad134af3bcc3a8ca94e4cb5e5d963b8e
oakfap <oakbahina@gmail.com>
2020-04-24 03:18:09 +0700

    sd

--- ex1/featureNormalize.m
+++ ex1/featureNormalize.m
@@ -26,9 +26,8 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-
-
-
+mu = [ mean(X(:,1)) , mean(X(:,2)) ]; 
+sigma = [ std(X(:,1)) , std(X(:,2)) ]; 
 
 
 
